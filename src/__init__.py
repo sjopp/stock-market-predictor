b1c44b8d22336ecaa610c745ab84e1b51473f498
Sam Jopp <samjopp@googlemail.com>
2020-05-14 17:31:40 +0100

    Adding token to request params

--- src/__init__.py
+++ src/__init__.py
@@ -4,7 +4,14 @@ import requests
 def get_stock_data(stock_symbol, start_date, end_date):
     url = f'https://api.tiingo.com/tiingo/daily/{stock_symbol}/prices?'
     params = {
+        'token': '387fd657063535f02ef5a5700aadd0b9286572e9',
         'startDate': start_date,
         'endDate': end_date
     }
     return requests.get(url, params).content
+
+
+if __name__ == '__main__':
+    aapl_data = get_stock_data('AAPL', '2019-12-25', '2020-1-1')
+    print(aapl_data)
+
