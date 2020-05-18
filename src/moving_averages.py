b09e6b2e653dfe8e1308f2164b6a98833d36c056
Sam Jopp <samjopp@googlemail.com>
2020-05-18 18:22:44 +0100

    Moving json file to resources folder

--- src/moving_averages.py
+++ src/moving_averages.py
@@ -1,6 +1,3 @@
-import pandas as pd
-
-
 def return_moving_average(dataframe):
     return dataframe.rolling(window=3).mean()
 
--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,5 @@
 from .context import src
+import json
 import unittest
 import responses
 import pandas.testing as pd_test
@@ -7,19 +8,8 @@ import pandas as pd
 
 class TestRetrievalOfData(unittest.TestCase):
 
-    json_expected = [
-        {'date': '2019-12-26T00:00:00.000Z', 'close': 289.91, 'high': 289.98, 'low': 284.7, 'open': 284.82,
-         'volume': 23334004, 'adjClose': 288.4514628933, 'adjHigh': 288.5211107234, 'adjLow': 283.2676744015,
-         'adjOpen': 283.3870706815, 'adjVolume': 23334004, 'divCash': 0.0, 'splitFactor': 1.0},
-        {'date': '2019-12-27T00:00:00.000Z', 'close': 289.8, 'high': 293.97, 'low': 288.12, 'open': 291.12,
-         'volume': 36592936, 'adjClose': 288.3420163033, 'adjHigh': 292.4910370348, 'adjLow': 286.6704683827,
-         'adjOpen': 289.6553753838, 'adjVolume': 36592936, 'divCash': 0.0, 'splitFactor': 1.0},
-        {'date': '2019-12-30T00:00:00.000Z', 'close': 291.52, 'high': 292.69, 'low': 285.22, 'open': 289.46,
-         'volume': 36059614, 'adjClose': 290.0533629839, 'adjHigh': 291.2174767143, 'adjLow': 283.7850582817,
-         'adjOpen': 288.0037268432, 'adjVolume': 36059614, 'divCash': 0.0, 'splitFactor': 1.0},
-        {'date': '2019-12-31T00:00:00.000Z', 'close': 293.65, 'high': 293.68, 'low': 289.52, 'open': 289.93,
-         'volume': 25247625, 'adjClose': 292.1726469547, 'adjHigh': 292.2024960247, 'adjLow': 288.0634249832,
-         'adjOpen': 288.4713622734, 'adjVolume': 25247625, 'divCash': 0.0, 'splitFactor': 1.0}]
+    with open('B:\\workspace\\trading\\stock-market-predictor\\tests\\resources\\AAPL_12_2019.json') as f:
+        json_expected = json.load(f)
 
     dataframe_expected = pd.DataFrame(json_expected,
                                       columns=['date', 'adjClose', 'adjHigh', 'adjLow', 'adjOpen',
