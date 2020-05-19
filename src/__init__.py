ecf4d932f7c379303c83ba3abd2d76071deb76b3
Sam Jopp <samjopp@googlemail.com>
2020-05-19 11:14:07 +0100

    Moving main code to call all methods into init file

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,13 @@
 import src.retrieve_convert as retrieve_and_convert
 import src.moving_averages as moving_averages
 
+
+def plot_data(stock, start_date, end_date, window):
+    json_data = retrieve_and_convert.get_stock_data(stock, start_date, end_date)
+    stock_dataframe = retrieve_and_convert.convert_to_dataframe(json_data)
+    simple_moving_averages = moving_averages.return_moving_average(stock_dataframe, window)
+    moving_averages.plot_moving_averages_against_stock_price(stock_dataframe, simple_moving_averages)
+
+
 if __name__ == '__main__':
     pass
--- tests/functional_test.py
+++ tests/functional_test.py
@@ -5,13 +5,7 @@ import unittest
 class FunctionalTest(unittest.TestCase):
 
     def test_end_to_end(self):
-        json_stock_data = src.retrieve_and_convert.get_stock_data('AAPL', '2019-1-1', '2020-1-1')
-        print(json_stock_data)
-        stock_dataframe = src.retrieve_and_convert.convert_to_dataframe(json_stock_data)
-        print(stock_dataframe)
-        moving_averages = src.moving_averages.return_moving_average(stock_dataframe, 5)
-        print(moving_averages)
-        src.moving_averages.plot_moving_averages_against_stock_price(stock_dataframe, moving_averages)
+        src.plot_data('AAPL', '2019-1-1', '2020-1-1', 10)
 
 
 if __name__ == '__main__':
