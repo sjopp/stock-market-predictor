e8f3516599a892d839a9ebdb055876d43400ffc9
Sam Jopp <samjopp@googlemail.com>
2020-05-27 14:47:59 +0100

    Testing MACD values

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -20,10 +20,10 @@ class TestRetrievalOfData(unittest.TestCase):
     def test_return_AAPL_current_stock_data_from_api(self):
         responses.add(responses.GET,
                       'https://api.tiingo.com/tiingo/daily/AAPL/prices?token'
-                      '=387fd657063535f02ef5a5700aadd0b9286572e9&startDate=2019-12-25&endDate=2020-1-1',
+                      '=387fd657063535f02ef5a5700aadd0b9286572e9&startDate=2019-12-1&endDate=2020-1-1',
                       json=self.resource, status=200)
 
-        json_actual = src.retrieve_and_convert.get_stock_data('AAPL', '2019-12-25', '2020-1-1')
+        json_actual = src.retrieve_and_convert.get_stock_data('AAPL', '2019-12-1', '2020-1-1')
 
         self.assertEqual(self.resource, json_actual)
 
@@ -42,7 +42,7 @@ class TestRetrievalOfData(unittest.TestCase):
 
         pd_test.assert_frame_equal(actual_moving_averages, dataframe_expected)
 
-    def test_exponential_moving_averages(self):
+    def test_exponential_slow_moving_averages(self):
         dataframe_expected = return_dataframe_resource('AAPL_12_2019_EMA_9_day.json')
 
         dataframe = src.retrieve_and_convert.convert_to_dataframe(self.resource)
@@ -51,8 +51,24 @@ class TestRetrievalOfData(unittest.TestCase):
         print(actual_dataframe)
         pd_test.assert_frame_equal(actual_dataframe, dataframe_expected)
 
+    def test_exponential_fast_moving_averages(self):
+        dataframe_expected = return_dataframe_resource('AAPL_12_2019_EMA_4_day.json')
+
+        dataframe = src.retrieve_and_convert.convert_to_dataframe(self.resource)
+        actual_dataframe = src.moving_averages.get_exponential_moving_averages(dataframe, 4)
+
+        print(actual_dataframe)
+        pd_test.assert_frame_equal(actual_dataframe, dataframe_expected)
+
     def test_mac_d_signal(self):
-        dataframe_expected = return_dataframe_resource('AAPL_MACD_signals.json')
+        responses.add(responses.GET,
+                      'https://api.tiingo.com/tiingo/daily/AAPL/prices?token'
+                      '=387fd657063535f02ef5a5700aadd0b9286572e9&startDate=2019-12-1&endDate=2020-1-1',
+                      json=self.resource, status=200)
+
+        result = src.mac_d.generate_mac_d_values('AAPL', '2019-12-1', '2020-1-1', 4, 9)
+        dataframe_expected = return_dataframe_resource('AAPL_MACD_values.json')
+        pd_test.assert_frame_equal(result, dataframe_expected)
 
 
 if __name__ == '__main__':
