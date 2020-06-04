636af736dd9521ccbd3b53c3c2eb82856f573381
Sam Jopp <samjopp@googlemail.com>
2020-06-04 17:14:53 +0100

    Separating ups and downs into separate columns and adding an smaUp column

--- src/rsi.py
+++ src/rsi.py
@@ -1,10 +1,24 @@
 import numpy as np
+import src.moving_averages as moving_averages
 
 
 def append_price_difference(price_dataframe):
-    differences = [np.float64(0)]
+    ups = [np.float64(0)]
+    downs = [np.float64(0)]
     for i in range(1, price_dataframe.shape[0]):
         difference = price_dataframe.iloc[i]['adjClose'] - price_dataframe.iloc[i - 1]['adjClose']
-        differences.append(difference)
-    price_dataframe['difference'] = differences
+        if difference >= 0:
+            ups.append(difference)
+            downs.append(np.float64(0))
+        else:
+            ups.append(np.float64(0))
+            downs.append(np.absolute(difference))
+    price_dataframe['ups'] = ups
+    price_dataframe['downs'] = downs
     return price_dataframe
+
+
+def append_sma(price_difference_dataframe):
+    sma_dataframe = moving_averages.get_simple_moving_averages(price_difference_dataframe, 9)
+    price_difference_dataframe['smaUp'] = sma_dataframe['ups']
+    return price_difference_dataframe
--- tests/unit_test_RSI.py
+++ tests/unit_test_RSI.py
@@ -4,12 +4,20 @@ import pandas.testing as pd_test
 
 
 class TestRSICalculations(unittest.TestCase):
+
     dataframe_AAPL_12_2019 = return_dataframe_resource('AAPL/AAPL_12_2019.json', ['date', 'adjClose'])
+    dataframe_AAPL_12_2019_price_differences = return_dataframe_resource('AAPL/AAPL_12_2019_RSI.json',
+                                                                         ['date', 'adjClose', 'ups', 'downs'])
 
     def test_we_return_price_difference_column_correctly(self):
-        expected_dataframe = return_dataframe_resource('AAPL/AAPL_12_2019_RSI_ups_and_downs.json',
-                                                       ['date', 'adjClose', 'difference'])
+        expected_dataframe = self.dataframe_AAPL_12_2019_price_differences
         actual_dataframe = src.rsi.append_price_difference(self.dataframe_AAPL_12_2019)
+        pd_test.assert_frame_equal(actual_dataframe, expected_dataframe)
+
+    def test_we_return_simple_moving_averages_column_correctly(self):
+        expected_dataframe = return_dataframe_resource('AAPL/AAPL_12_2019_RSI.json',
+                                                       ['date', 'adjClose', 'ups', 'downs', 'smaUp'])
+        actual_dataframe = src.rsi.append_sma(self.dataframe_AAPL_12_2019_price_differences)
         pd_test.assert_frame_equal(actual_dataframe, expected_dataframe)
 
 
